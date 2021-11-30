bbac0b4dadb347247a025e37b0aeceb8c5e768a8
lensu <lensu@lensu.ink>
2021-11-30 21:46:03 +0800

    spring initialize commit

---
+++ src/main/java/com/poindre/shua/ShuaApplication.java
@@ -0,0 +1,13 @@
+package com.poindre.shua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShuaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShuaApplication.class, args);
+    }
+
+}
